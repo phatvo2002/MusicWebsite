5a36500f7f4bc79bb12bb58bf25cc11d5b08ae98
Huan Tran <Admin@huan>
2024-10-18 00:08:59 +0700

    update

--- Client/my-MusicWebsite/src/Components/MenuContent.jsx
+++ Client/my-MusicWebsite/src/Components/MenuContent.jsx
@@ -1,60 +1,61 @@
-
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Stack from '@mui/material/Stack';
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import AnalyticsRoundedIcon from '@mui/icons-material/AnalyticsRounded';
-import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
-import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
-import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
-import { Typography } from '@mui/material';
-import {Link} from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Stack from "@mui/material/Stack";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import AnalyticsRoundedIcon from "@mui/icons-material/AnalyticsRounded";
+import PeopleRoundedIcon from "@mui/icons-material/PeopleRounded";
+import AssignmentRoundedIcon from "@mui/icons-material/AssignmentRounded";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
+import HelpRoundedIcon from "@mui/icons-material/HelpRounded";
+import { Typography } from "@mui/material";
+import { Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 const mainListItems = [
-  { text: 'Home', icon: <HomeRoundedIcon />  },
-  { text: 'Discover', icon: <AnalyticsRoundedIcon /> },
-  { text: 'Albums', icon: <PeopleRoundedIcon /> },
-  { text: 'Artits', icon: <AssignmentRoundedIcon /> },
+  { text: "Home", icon: <HomeRoundedIcon />, url: "/Home" },
+  { text: "Discover", icon: <AnalyticsRoundedIcon />, url: "/" },
+  { text: "Albums", icon: <PeopleRoundedIcon />, url: "/" },
+  { text: "Artits", icon: <AssignmentRoundedIcon />, url: "/" },
 ];
 
 const secondaryListItems = [
-  { text: 'Recently Added', icon: <SettingsRoundedIcon /> },
-  { text: 'Most played', icon: <InfoRoundedIcon /> },
+  { text: "Recently Added", icon: <SettingsRoundedIcon /> },
+  { text: "Most played", icon: <InfoRoundedIcon /> },
 ];
 
 const thirdListItems = [
-  { text: 'Your favotites', icon: <SettingsRoundedIcon /> },
-  { text: 'Your playlist', icon: <InfoRoundedIcon /> },
-  { text: 'Add playlist', icon: <HelpRoundedIcon /> },
+  { text: "Your favotites", icon: <SettingsRoundedIcon /> },
+  { text: "Your playlist", icon: <InfoRoundedIcon /> },
+  { text: "Add playlist", icon: <HelpRoundedIcon /> },
 ];
 const fordListItems = [
-  { text: 'Settings', icon: <SettingsRoundedIcon /> , url : "/"  },
-  { text: 'Administrator', icon: <InfoRoundedIcon /> , url : "/Administrator" },
+  { text: "Settings", icon: <SettingsRoundedIcon />, url: "/" },
+  { text: "Administrator", icon: <InfoRoundedIcon />, url: "/Administrator" },
 ];
 export default function MenuContent() {
   return (
-    <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
+    <Stack sx={{ flexGrow: 1, p: 1, justifyContent: "space-between" }}>
       <List dense>
         {mainListItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+          <ListItem key={index} disablePadding sx={{ display: "block" }}>
             <ListItemButton selected={index === 0}>
               <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.text} />
+              <Link component={RouterLink} to={item.url}>
+                {item.text}
+              </Link>
             </ListItemButton>
           </ListItem>
         ))}
       </List>
-      <Typography style={{color :"#f010ae", paddingLeft:20}}>
-         Libary
+      <Typography style={{ color: "#f010ae", paddingLeft: 20 }}>
+        Libary
       </Typography>
       <List dense>
         {secondaryListItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+          <ListItem key={index} disablePadding sx={{ display: "block" }}>
             <ListItemButton>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.text} />
@@ -62,12 +63,12 @@ export default function MenuContent() {
           </ListItem>
         ))}
       </List>
-      <Typography style={{color :"#f010ae", paddingLeft:20}}>
-         Laylist and Favorites
+      <Typography style={{ color: "#f010ae", paddingLeft: 20 }}>
+        Laylist and Favorites
       </Typography>
       <List dense>
         {thirdListItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+          <ListItem key={index} disablePadding sx={{ display: "block" }}>
             <ListItemButton>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.text} />
@@ -75,16 +76,18 @@ export default function MenuContent() {
           </ListItem>
         ))}
       </List>
-      <Typography style={{color :"#f010ae", paddingLeft:20}} >
-         General
+      <Typography style={{ color: "#f010ae", paddingLeft: 20 }}>
+        General
       </Typography>
       <List dense>
         {fordListItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+          <ListItem key={index} disablePadding sx={{ display: "block" }}>
             <ListItemButton>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText>
-                    <Link component={RouterLink} to={item.url}>{item.text}</Link>
+                <Link component={RouterLink} to={item.url}>
+                  {item.text}
+                </Link>
               </ListItemText>
             </ListItemButton>
           </ListItem>
--- Client/my-MusicWebsite/src/routes.jsx
+++ Client/my-MusicWebsite/src/routes.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from './View/RootLayout/Dashboard';
-import Administrator from './View/MainComponent/Administrator/Administrator';
-import TheLoaiNhac from './View/MainComponent/Administrator/TheLoai/TheLoaiNhac';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./View/RootLayout/Dashboard";
+import Administrator from "./View/MainComponent/Administrator/Administrator";
+import TheLoaiNhac from "./View/MainComponent/Administrator/TheLoai/TheLoaiNhac";
+import Home from "./View/MainComponent/Home/Home";
 const AppRoutes = () => {
-    return (
-      <Routes>
-        <Route path="/" element={<Dashboard />}>
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />}>
+        <Route path="/Home" element={<Home />} />
         <Route path="/Administrator" element={<Administrator />} />
         <Route path="/Administrator/TheLoaiNhac" element={<TheLoaiNhac />} />
         {/* Thêm các route con khác */}
-       </Route>
-      </Routes>
-    );
-  };
-  
-  export default AppRoutes;
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
